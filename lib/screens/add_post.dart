a617beed4c8639f0468d233b9e45a390fac9c84f
im-nilesh <nileshdadab@gmail.com>
2024-05-02 12:48:09 +0530

    Add post Ui complete

---
+++ lib/screens/add_post.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/colors.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // return Center(
+    //   child: IconButton(
+    //     icon: const Icon(Icons.upload),
+    //     onPressed: () {},
+    //   ),
+    // );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text('Post to'),
+        centerTitle: false,
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Post',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ))
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://st4.depositphotos.com/1000423/23971/i/450/depositphotos_239719906-stock-photo-networking-as-global-concept.jpg'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Write a caption...',
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://st4.depositphotos.com/1000423/23971/i/450/depositphotos_239719906-stock-photo-networking-as-global-concept.jpg'),
+                          fit: BoxFit.fill,
+                          alignment: FractionalOffset.topCenter),
+                    ),
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
