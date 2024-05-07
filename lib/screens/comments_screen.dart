dcaab80e9e854b532dd3bdb8e12179959d8e8f4e
im-nilesh <nileshdadab@gmail.com>
2024-05-07 16:46:54 +0530

    User comment ui

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -19,21 +19,46 @@ class _CommentsScreenState extends State<CommentsScreen> {
       ),
       bottomNavigationBar: SafeArea(
         child: Container(
-            height: kToolbarHeight,
-            margin: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-            ),
-            padding: const EdgeInsets.only(left: 16, right: 8),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1712606992721-fbb5d3b70aee?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D',
-                  ),
-                  radius: 18,
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1712606992721-fbb5d3b70aee?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D',
                 ),
-              ],
-            )),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Comment as username',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 8,
+                  ),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
